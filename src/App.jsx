834e681fcdb3e48ea3a3f6d4777dba91453f86ee
Tabiullah Khan <khantabiullah1@gmail.com>
2025-01-30 14:54:13 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+
+import About from './components/About'
+import Dishes from './components/Dishes'
+import HeroSection from './components/HeroSection'
+import Navbar from './components/Navbar'
+import Mission from './components/Mission'
+import Expertise from './components/Expertise'
+import Review from './components/Review'
+import ContactSection from './components/ContactSection'
+import Footer from './components/Footer'
+
+function App() {
+  return (
+    <main className=' overflow-y-hidden text-neutral-200 antialiased'> 
+      <HeroSection />
+      <Navbar />
+      <Dishes />
+      <About/>
+      <Mission />
+      <Expertise />
+      <Review />
+      <ContactSection />
+      <Footer />
+  </main>
+  )
+}
+
+export default App
---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,25 @@
+import video from "../assets/hero.mp4"
+import logo from "../assets/logo.png"
+import hero from "../assets/hero.jpeg"
+import { motion } from "framer-motion"
+function HeroSection() {
+  return (
+    <section className="relative flex h-screen item-center justify-center">
+        <div className="absolute inset-0 -z-20 h-full w-full overflow-hidden">
+            <video src={video} className="h-full w-full object-cover" muted autoPlay loop playsInline poster={hero}></video>
+        </div>
+        <div className="absolute inset-0 -z-10 
+        bg-gradiant-to-b from-transparent from-70% to-black"></div>
+        <div className="relative z-20 flex h-screen flex-col  justify-end pb-20">
+
+       <motion.img initial={{opacity: 0 , y: 50}}
+         animate={{opacity: 1, y: 0}}
+         transition={{duration: 0.5}}
+       src={logo} alt="restaurent" className="w-full  p-4" />
+       <p className="p-4 text-lg tracking-tighter text-white">Paris</p>
+        </div>
+    </section>
+  )
+}
+
+export default HeroSection  
